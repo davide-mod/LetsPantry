d977294ec37f052cfc22ae461394331dfd92bda0
capezzoly <davide.modolo98@gmail.com>
2020-08-25 00:45:08 +0200

    cambiata libreria calendario, aggiunto TODO

--- app/src/main/java/com/modolodavide/letspantry/ui/main/MainFragment.kt
+++ app/src/main/java/com/modolodavide/letspantry/ui/main/MainFragment.kt
@@ -2,21 +2,20 @@ package com.modolodavide.letspantry.ui.main
 
 import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.core.view.isVisible
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getColor
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.github.sundeepk.compactcalendarview.CompactCalendarView
-import com.github.sundeepk.compactcalendarview.CompactCalendarView.CompactCalendarViewListener
-import com.github.sundeepk.compactcalendarview.domain.Event
 import com.modolodavide.letspantry.R
-import kotlinx.android.synthetic.main.main_fragment.*
-import java.time.YearMonth
-import java.util.*
+import sun.bob.mcalendarview.MarkStyle
+import sun.bob.mcalendarview.listeners.OnMonthChangeListener
+import sun.bob.mcalendarview.views.ExpCalendarView
+import sun.bob.mcalendarview.vo.DateData
+
 
 val TAG = "debuggalo"
 
@@ -35,51 +34,42 @@ class MainFragment : Fragment() {
     ): View {
         val view = inflater.inflate(R.layout.main_fragment, container, false)
 
+        /*
+        TODO
+        FUNZIONALITA'
+        - Creare db ingredienti 30mins
+        - Leggo il db per caricare la lista di ingredienti in casa 30mins
+        - ciclo la lista per segnare nel calendario le date di scadenza
+        - data.clickListener appare finestra con gli ingredienti che scadono in quel giorno 30mins
+            -> query con ricerca per giorno
+        - finestra per aggiunta ingredienti 1h
+        - menu laterale per cambiare Fragment 30mins
+        - order by ricerca (stile gmail)
+        DESIGN:
+        - pulsante per nascondere il calendario 5mins
+        - nome del mese: prendo il mese attuale dal sistema, poi onMonthChangeListener 15mins
 
+        PARTE 2(h):
+        - db lista della spesa
+        - aggiunta sequenziale
+        - longpress: menu: elimina, manda a db ingredenti, rimetti in lista
 
-        val txtCalendario: TextView = view.findViewById(R.id.txtCalendario)
-        val calendario: CompactCalendarView = view.findViewById(R.id.compactcalendar_view)
-        calendario.setLocale(Calendar.getInstance().timeZone, Locale.ITALY)
-        calendario.setUseThreeLetterAbbreviation(true)
-        calendario.setFirstDayOfWeek(Calendar.MONDAY)
-        calendario.isVisible = false
+        PARTE 3:
+        - db ricette 30min
+        - ricerca per ingredienti disponibili rimossi all'uso 4h
 
-        val ev1 = Event(Color.GREEN, 1598392800000, "Some extra data that I want to store.")
-        calendario.addEvent(ev1)
-        val ev2 = Event(Color.GREEN, 1598392800000)
-        calendario.addEvent(ev2)
-        // Query for events on Sun, 07 Jun 2015 GMT.
-        // Time is not relevant when querying for events, since events are returned by day.
-        // So you can pass in any arbitary DateTime and you will receive all events for that day.
-        val events: List<Event> =
-            calendario.getEvents(1598392800000) // can also take a Date object
-        // events has size 2 with the 2 events inserted previously
-        Log.d(TAG, "Events: $events")
-        // define a listener to receive callbacks when certain events happen.
-        calendario.setListener(object : CompactCalendarViewListener {
-            override fun onDayClick(dateClicked: Date) {
-                calendario.setCurrentSelectedDayBackgroundColor(Color.parseColor("#B2FF59"))
-                val events: List<Event> = calendario.getEvents(dateClicked)
-                Log.d(TAG, "Day was clicked: $dateClicked with events $events")
-            }
+        TOT: 10h
+         */
+        //https://github.com/SpongeBobSun/mCalendarView
+        val calendario: ExpCalendarView = view.findViewById(R.id.calendar_exp)
 
-            override fun onMonthScroll(firstDayOfNewMonth: Date) {
-                calendario.setCurrentSelectedDayBackgroundColor(Color.parseColor("#00E676"))
-                txtCalendario.text = firstDayOfNewMonth.toString()
-                Log.d(TAG, "Month was scrolled to: $firstDayOfNewMonth")
+        calendario.markDate(DateData(2020, 8, 27).setMarkStyle(MarkStyle.BACKGROUND, getColor(context!!, R.color.colorPrimary)))
+
+        calendario.setOnMonthChangeListener(object : OnMonthChangeListener() {
+            override fun onMonthChange(year: Int, month: Int) {
+                //cambio campo Mese con $month
             }
         })
-
-        txtCalendario.setOnClickListener {
-            if(calendario.isVisible && !calendario.isAnimating){
-                calendario.hideCalendarWithAnimation()
-                calendario.isVisible=false
-            }
-            else {
-                calendario.showCalendarWithAnimation()
-                calendario.isVisible = true
-            }
-        }
 
         return view
     }
